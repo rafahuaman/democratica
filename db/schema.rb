5fd309d9b0b6e22ff23bc7146625486e67488428
Rafael <leafar.huaman@gmail.com>
2014-08-31 21:35:39 +0000

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140829182553) do
+ActiveRecord::Schema.define(version: 20140831212247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,17 @@ ActiveRecord::Schema.define(version: 20140829182553) do
     t.datetime "updated_at"
     t.string   "type"
   end
+
+  create_table "rallies", force: true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.string   "twitter_template"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "rallies", ["user_id"], name: "index_rallies_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "name"
