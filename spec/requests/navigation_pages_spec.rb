f2873b9de465f3cf15898402bacae0ca4875b806
Rafael <leafar.huaman@gmail.com>
2014-09-01 18:08:18 +0000

    modify sidebar

--- spec/requests/navigation_pages_spec.rb
+++ spec/requests/navigation_pages_spec.rb
@@ -9,15 +9,13 @@ describe "Navigation Requests Spec" do
       should have_link('Home')
       should have_link('About')
       should have_link('Sign up')
-      should have_link('Start a new rally')
     end
   end
   
   describe "Side bar should be available" do
     it "should show the sidebar on all pages" do
       visit root_path
-      #should have_link('Submit a new rally', new_debate_path)
-      #should have_link('Create a new chamber', new_chamber_path)
+      should have_link('Submit a new rally', new_rally_path)
     end
   end
 end
