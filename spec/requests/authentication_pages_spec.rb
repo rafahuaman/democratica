b6ebde16ddb4b04766fab9d0884027a72178db6f
Rafael <leafar.huaman@gmail.com>
2014-09-01 03:17:55 +0000

    add Rally authorization tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -111,22 +111,74 @@ describe "Authentication" do
           specify { expect(response).to redirect_to(signin_path) }
         end
       end
+
+      describe "in the Rally controller" do
+        let(:rally) { FactoryGirl.create(:rally) }
+
+        describe "visiting the new page" do
+          before { visit new_rally_path }
+          it { should have_title('Sign in') }
+        end
+
+        describe "visiting the edit page" do
+          before { visit edit_rally_path(rally) }
+          it { should have_title('Sign in') }
+        end
+
+        describe "submitting to the update action" do
+          before { patch rally_path(rally) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the create action" do
+          before { post rallies_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete rally_path(rally) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+      end
     end
 
     describe "as wrong user" do
-      let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
-      before { sign_in user, no_capybara: true }
+      describe "in the Users controller" do
+        #let(:user) { FactoryGirl.create(:user) }
+        
+        before { sign_in user, no_capybara: true }
 
-      describe "submitting a GET request to the Users#edit action" do
-        before { get edit_user_path(wrong_user) }
-        specify { expect(response.body).not_to match('Edit user') }
-        specify { expect(response).to redirect_to(root_url) }
+        describe "submitting a GET request to the Users#edit action" do
+          before { get edit_user_path(wrong_user) }
+          specify { expect(response.body).not_to match('Edit user') }
+          specify { expect(response).to redirect_to(root_url) }
+        end
+
+        describe "submitting a PATCH request to the Users#update action" do
+          before { patch user_path(wrong_user) }
+          specify { expect(response).to redirect_to(root_url) }
+        end
       end
 
-      describe "submitting a PATCH request to the Users#update action" do
-        before { patch user_path(wrong_user) }
-        specify { expect(response).to redirect_to(root_url) }
+      describe "in the Rallies controller" do
+        before { sign_in user, no_capybara: true }
+
+        describe "submitting a GET request to the #edit action" do
+          before { get edit_rally_path(rally) }
+          it { should respond_by_redirecting_to_root_page }
+        end
+        
+        describe "submitting a PATCH request to the #update action" do
+          before { patch rally_path(rally) }
+          it { should respond_by_redirecting_to_root_page }
+        end
+        
+        describe "submitting a DELETE request to the #delete action" do
+          before { delete rally_path(debate) }
+          it { should respond_by_redirecting_to_root_page }
+        end
       end
     end
 
