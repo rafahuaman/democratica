8106edee8e8276f03d78bb7f2a888511d83d287b
Rafael <leafar.huaman@gmail.com>
2014-10-30 12:53:08 +0000

    Tweet from App

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,13 @@
 class TweetsController < ApplicationController
   def new
+
   end
 
   def create
+    current_user.tweet(twitter_params[:message])
+  end
+
+  def twitter_params
+    params.require(:tweet).permit(:message)
   end
 end
