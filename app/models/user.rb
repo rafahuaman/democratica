282b9f459befb1defe7fefa88a29b905e4ce5242
Rafael <leafar.huaman@gmail.com>
2014-11-18 13:15:13 +0000

    wrapped tweet method inside in TweetAdatper

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,14 +19,7 @@ class User < ActiveRecord::Base
   end
 
   def tweet(tweet)
-    client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.config.twitter_key
-      config.consumer_secret     = Rails.application.config.twitter_secret
-      config.access_token        = identity.access_token
-      config.access_token_secret = identity.access_secret
-    end
-    
-    client.update(tweet)
+    TwitterAdapter.tweet(tweet, identity.access_token, identity.access_secret)
   end
 
   def support(rally)
--- lib/twitter_adapter.rb
+++ lib/twitter_adapter.rb
@@ -1,6 +1,13 @@
 class TwitterAdapter
   
-def tweet(tweet)
-  
-end
+  def self.tweet(tweet, access_token, access_secret)
+     client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = Rails.application.config.twitter_key
+      config.consumer_secret     = Rails.application.config.twitter_secret
+      config.access_token        = access_token
+      config.access_token_secret = access_secret
+    end
+    
+    client.update(tweet)
+  end
 end
