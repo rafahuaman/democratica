74f0b0258684b11799491ac1b8c75352b0ec0f9e
Rafael <leafar.huaman@gmail.com>
2014-12-10 13:16:47 +0000

    add migration to add parent_id to comments and added maintain_test_schema to spec_helper

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141202140028) do
+ActiveRecord::Schema.define(version: 20141210131025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20141202140028) do
     t.integer  "rally_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "parent_id"
   end
 
   add_index "comments", ["rally_id", "user_id", "created_at"], name: "index_comments_on_rally_id_and_user_id_and_created_at", using: :btree
